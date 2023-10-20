b68daeb9c59ebf7595e6b9d65775d945b7bf1897
Sahnawaz Shaban <shaanshaban400@gmail.com>
2023-10-20 15:34:33 +0530

    Largest Prime Factor

---
+++ GFG/13) Largest prime factor.py
@@ -0,0 +1,55 @@
+"""
+Largest prime factor
+
+Given a number N, the task is to find the largest prime factor of that number.
+ Example 1:
+
+Input:
+N = 5
+Output:
+5
+Explanation:
+5 has 1 prime factor i.e 5 only.
+Example 2:
+
+Input:
+N = 24
+Output:
+3
+Explanation:
+24 has 2 prime factors 2 and 3 in which 3 is greater.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function largestPrimeFactor() which takes an integer N as input parameters and returns an integer, largest prime factor of N.
+
+Expected Time Complexity: O(sqrt(N))
+Expected Space Complexity: O(1)
+
+Constraints:
+2 <= N <= 109
+"""
+
+# SOLUTION
+
+import math
+
+class Solution:
+    def largestPrimeFactor (self, N):
+        # code here
+        largest_prime = -1  # Initialize the largest prime factor to -1.
+
+        # Divide N by 2 until it's even.
+        while N % 2 == 0:
+            largest_prime = 2
+            N //= 2
+    
+        # Check for prime factors starting from 3.
+        for i in range(3, int(math.sqrt(N)) + 1, 2):
+            while N % i == 0:
+                largest_prime = i
+                N //= i
+    
+        # If N is still greater than 2, it must be prime.
+        if N > 2:
+            largest_prime = N
+    
+        return largest_prime

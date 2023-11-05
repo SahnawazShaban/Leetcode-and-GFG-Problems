64bc593ded7cd3fe5ae44aadfcf414cafbc0578c
Sahnawaz Shaban <shaanshaban400@gmail.com>
2023-11-05 23:00:23 +0530

    Sort array with square

---
+++ Interview Bit/1) Sort array with squares.py
@@ -0,0 +1,85 @@
+"""
+Sort array with squares!
+
+Problem Description
+
+Given a sorted array A containing N integers both positive and negative.
+
+You need to create another array containing the squares of all the elements in A and return it in non-decreasing order.
+
+Try to this in O(N) time.
+
+
+Problem Constraints
+1 <= N <= 105.
+
+-103 <= A[i] <= 103
+
+
+
+Input Format
+First and only argument is an integer array A.
+
+
+
+Output Format
+Return a integer array as described in the problem above.
+
+
+
+Example Input
+Input 1:
+
+ A = [-6, -3, -1, 2, 4, 5]
+Input 2:
+
+ A = [-5, -4, -2, 0, 1]
+
+
+Example Output
+Output 1:
+
+ [1, 4, 9, 16, 25, 36]
+Output 2:
+
+ [0, 1, 4, 16, 25]
+
+"""
+
+# SOLUTION
+
+class Solution:
+    # @param A : list of integers
+    # @return a list of integers
+    def solve(self, A):
+        # ans = []
+        
+        # for i in range(len(A)):
+        #     val = A[i]*A[i]
+        #     ans.append(val)
+            
+        # ans.sort()
+            
+        # return ans
+        
+        # ---------------------------------
+        
+        n = len(A)
+        result = [0] * n
+        left, right = 0, n - 1
+        index = n - 1
+
+        while left <= right:
+            left_square = A[left] * A[left]
+            right_square = A[right] * A[right]
+
+            if left_square > right_square:
+                result[index] = left_square
+                left += 1
+            else:
+                result[index] = right_square
+                right -= 1
+
+            index -= 1
+
+        return result

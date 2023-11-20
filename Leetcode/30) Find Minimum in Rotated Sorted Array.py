fe70c79119fcd25940fe1863050f36f6db3755db
Sahnawaz Shaban <shaanshaban400@gmail.com>
2023-11-20 14:22:24 +0530

    Find Minimum in Rotated Sorted Array

---
+++ Leetcode/30) Find Minimum in Rotated Sorted Array.py
@@ -0,0 +1,142 @@
+"""
+153. Find Minimum in Rotated Sorted Array
+
+Medium
+
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+[4,5,6,7,0,1,2] if it was rotated 4 times.
+[0,1,2,4,5,6,7] if it was rotated 7 times.
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+You must write an algorithm that runs in O(log n) time.
+
+ 
+
+Example 1:
+Input: nums = [3,4,5,1,2]
+Output: 1
+Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+
+Example 2:
+Input: nums = [4,5,6,7,0,1,2]
+Output: 0
+Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+
+Example 3:
+Input: nums = [11,13,15,17]
+Output: 11
+Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 5000
+-5000 <= nums[i] <= 5000
+All the integers of nums are unique.
+nums is sorted and rotated between 1 and n times.
+
+"""
+
+# SOLUTION
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        ## Two Pointer Approach
+        n = len(nums)
+        l, r =0, n-1
+        min_ele = float('inf')
+
+        while l <= r:
+            if nums[l] <= nums[r]:
+                if min_ele > nums[l]:
+                    min_ele = nums[l]
+                l += 1
+                r -= 1
+            else:
+                if min_ele > nums[r]:
+                    min_ele = nums[r]
+                l += 1
+                r -= 1
+        return min_ele
+
+        # --------------------------------
+
+        ## Binary Search
+        # set left and right bounds
+        left, right = 0, len(nums)-1
+                
+        # left and right both converge to the minimum index;
+        # DO NOT use left <= right because that would loop forever
+        while left < right:
+            # find the middle value between the left and right bounds (their average);
+			# can equivalently do: mid = left + (right - left) // 2,
+			# if we are concerned left + right would cause overflow (which would occur
+			# if we are searching a massive array using a language like Java or C that has
+			# fixed size integer types)
+            mid = (left + right) // 2
+                        
+            # the main idea for our checks is to converge the left and right bounds on the start
+            # of the pivot, and never disqualify the index for a possible minimum value.
+
+            # in normal binary search, we have a target to match exactly,
+            # and would have a specific branch for if nums[mid] == target.
+            # we do not have a specific target here, so we just have simple if/else.
+                        
+            if nums[mid] > nums[right]:
+                # we KNOW the pivot must be to the right of the middle:
+                # if nums[mid] > nums[right], we KNOW that the
+                # pivot/minimum value must have occurred somewhere to the right
+                # of mid, which is why the values wrapped around and became smaller.
+
+                # example:  [3,4,5,6,7,8,9,1,2] 
+                # in the first iteration, when we start with mid index = 4, right index = 9.
+                # if nums[mid] > nums[right], we know that at some point to the right of mid,
+                # the pivot must have occurred, which is why the values wrapped around
+                # so that nums[right] is less then nums[mid]
+
+                # we know that the number at mid is greater than at least
+                # one number to the right, so we can use mid + 1 and
+                # never consider mid again; we know there is at least
+                # one value smaller than it on the right
+                left = mid + 1
+
+            else:
+                # here, nums[mid] <= nums[right]:
+                # we KNOW the pivot must be at mid or to the left of mid:
+                # if nums[mid] <= nums[right], we KNOW that the pivot was not encountered
+                # to the right of middle, because that means the values would wrap around
+                # and become smaller (which is caught in the above if statement).
+                # this leaves the possible pivot point to be at index <= mid.
+                            
+                # example: [8,9,1,2,3,4,5,6,7]
+                # in the first iteration, when we start with mid index = 4, right index = 9.
+                # if nums[mid] <= nums[right], we know the numbers continued increasing to
+                # the right of mid, so they never reached the pivot and wrapped around.
+                # therefore, we know the pivot must be at index <= mid.
+
+                # we know that nums[mid] <= nums[right].
+                # therefore, we know it is possible for the mid index to store a smaller
+                # value than at least one other index in the list (at right), so we do
+                # not discard it by doing right = mid - 1. it still might have the minimum value.
+                right = mid
+                
+        # at this point, left and right converge to a single index (for minimum value) since
+        # our if/else forces the bounds of left/right to shrink each iteration:
+
+        # when left bound increases, it does not disqualify a value
+        # that could be smaller than something else (we know nums[mid] > nums[right],
+        # so nums[right] wins and we ignore mid and everything to the left of mid).
+
+        # when right bound decreases, it also does not disqualify a
+        # value that could be smaller than something else (we know nums[mid] <= nums[right],
+        # so nums[mid] wins and we keep it for now).
+
+        # so we shrink the left/right bounds to one value,
+        # without ever disqualifying a possible minimum
+        return nums[left]
+
+        

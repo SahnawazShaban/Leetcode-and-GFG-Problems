88270b337b4b5b44d34d3138a249cbb0623b6706
Sahnawaz Shaban <shaanshaban400@gmail.com>
2023-12-07 19:14:43 +0530

    Contest

---
+++ Daily Leetcode Challenge/5) Largest Odd Number in String.py
@@ -0,0 +1,61 @@
+"""
+1903. Largest Odd Number in String
+
+Easy
+
+You are given a string num, representing a large integer. Return the largest-valued odd integer (as a string) that is a non-empty substring of num, or an empty string "" if no odd integer exists.
+
+A substring is a contiguous sequence of characters within a string.
+
+ 
+Example 1:
+Input: num = "52"
+Output: "5"
+Explanation: The only non-empty substrings are "5", "2", and "52". "5" is the only odd number.
+
+Example 2:
+Input: num = "4206"
+Output: ""
+Explanation: There are no odd numbers in "4206".
+
+Example 3:
+Input: num = "35427"
+Output: "35427"
+Explanation: "35427" is already an odd number.
+ 
+
+Constraints:
+
+1 <= num.length <= 10^5
+num only consists of digits and does not contain any leading zeros.
+
+"""
+
+
+# Solution 
+
+class Solution:
+    def largestOddNumber(self, num: str) -> str:
+        # Solution : 1
+        '''
+        n = len(num)-1
+        while n >= 0:
+            val = int(num[n])
+            if val % 2 == 1:
+                return num[:n+1]
+            n -= 1
+        
+        return ""
+        '''
+        # ------------------------------
+
+        # Solution : 2
+        
+        for i in range(len(num)-1,-1,-1):
+            if num[i] in {'1','3','5','7','9'}:
+                # slicing - list1[include : exclude]
+                return num[:i+1]
+        
+        return ""
+
+        

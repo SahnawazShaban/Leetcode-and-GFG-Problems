9eb582e68df122c4248628a24cf76aa6873241f5
Sahnawaz Shaban <shaanshaban400@gmail.com>
2023-12-13 01:44:29 +0530

    Day - 12

---
+++ Daily Leetcode Challenge/10) Maximum Product of Two Elements in an Array.py
@@ -0,0 +1,61 @@
+"""
+1464. Maximum Product of Two Elements in an Array
+
+Easy
+
+Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
+
+Example 1:
+Input: nums = [3,4,5,2]
+Output: 12 
+Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12. 
+
+Example 2:
+Input: nums = [1,5,4,5]
+Output: 16
+Explanation: Choosing the indices i=1 and j=3 (indexed from 0), you will get the maximum value of (5-1)*(5-1) = 16.
+
+Example 3:
+Input: nums = [3,7]
+Output: 12
+
+Constraints:
+
+2 <= nums.length <= 500
+1 <= nums[i] <= 10^3
+
+"""
+
+
+# Solution 
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        # Solution - 1 - Brute Force
+        '''
+        n = len(nums)
+        maxi = float('-inf')
+
+        for i in range(n):
+            for j in range(i+1,n):
+                maxi = max(maxi, (nums[i]-1)*(nums[j]-1))
+        
+        return maxi
+        '''
+
+        # ----------------------------------------
+
+        # Solution - 2 - Better Approach
+
+        first, second = 0, 0
+        for number in nums:
+            if number > first:
+                # update first largest and second largest
+                first, second = number, first
+                
+            elif number > second:
+                # update second largest
+                second = number
+        
+        return (first - 1) * (second - 1)
+    

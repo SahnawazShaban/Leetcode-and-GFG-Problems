b77f2aa9b1348888630c5c42d7c1a9ebfff417d0
Sahnawaz Shaban <shaanshaban400@gmail.com>
2024-01-23 17:10:12 +0530

    Basics of Graph

--- Step-Up/Graph/0) Adjacency Matrix and List.py
+++ Step-Up/Graph/0) Adjacency Matrix and List.py
@@ -1,32 +1,33 @@
-def adjacencyMatrix(ipt,mg):
-    for i in range(len(ipt)):
+def adjacencyMatrix(ipt, n, mg):
+    for i in range(n):
         temp = []
-        for j in range(len(ipt)):
+        for j in range(n):
             temp.append(0)
         mg.append(temp)
 
-    for (u,v) in ipt:
+    for (u, v) in ipt:
         mg[u][v] = 1
         mg[v][u] = 1
 
     return mg
 
-def adjacencyList(ipt, graph):
-    for i in range(len(ipt)):
+
+def adjacencyList(ipt, n, graph):
+    for i in range(n):
         graph[i] = []
 
-    for (u,v) in ipt:
+    for (u, v) in ipt:
         graph[u].append(v)
         graph[v].append(u)
 
 
-ipt = [[0,1], [0,2], [0,3], [0,4], [1,3], [2,3], [2,4], [2,5], [3,5]]
+ipt = [[0, 1], [0, 2], [0, 3], [0, 4], [1, 3], [2, 3], [2, 4], [2, 5], [3, 5]]
 
 print("Adjacency Matrix")
 # Adjacency Matrix
 mg = []
-
-adjacencyMatrix(ipt,mg)
+n = 6
+adjacencyMatrix(ipt, n, mg)
 
 for row in mg:
     print(row)
@@ -35,8 +36,8 @@ print("--------------------------------------")
 print("Adjacency List")
 # Adjacency List
 graph = {}
-
-adjacencyList(ipt, graph)
+n = 6
+adjacencyList(ipt, n, graph)
 
 for row in graph.items():
     print(row)

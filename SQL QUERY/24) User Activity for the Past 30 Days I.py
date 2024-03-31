5a7195d2461a312120175266b2ab3a591f04c845
Sahnawaz Shaban <shaanshaban400@gmail.com>
2024-03-31 20:59:46 +0530

    Easy

---
+++ SQL QUERY/24) User Activity for the Past 30 Days I.py
@@ -0,0 +1,78 @@
+"""
+1141. User Activity for the Past 30 Days I
+
+Easy
+
+Table: Activity
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| user_id       | int     |
+| session_id    | int     |
+| activity_date | date    |
+| activity_type | enum    |
++---------------+---------+
+This table may have duplicate rows.
+The activity_type column is an ENUM (category) of type ('open_session', 'end_session', 'scroll_down', 'send_message').
+The table shows the user activities for a social media website. 
+Note that each session belongs to exactly one user.
+ 
+
+Write a solution to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively. A user was active on someday if they made at least one activity on that day.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+
+Example 1:
+Input: 
+Activity table:
++---------+------------+---------------+---------------+
+| user_id | session_id | activity_date | activity_type |
++---------+------------+---------------+---------------+
+| 1       | 1          | 2019-07-20    | open_session  |
+| 1       | 1          | 2019-07-20    | scroll_down   |
+| 1       | 1          | 2019-07-20    | end_session   |
+| 2       | 4          | 2019-07-20    | open_session  |
+| 2       | 4          | 2019-07-21    | send_message  |
+| 2       | 4          | 2019-07-21    | end_session   |
+| 3       | 2          | 2019-07-21    | open_session  |
+| 3       | 2          | 2019-07-21    | send_message  |
+| 3       | 2          | 2019-07-21    | end_session   |
+| 4       | 3          | 2019-06-25    | open_session  |
+| 4       | 3          | 2019-06-25    | end_session   |
++---------+------------+---------------+---------------+
+Output: 
++------------+--------------+ 
+| day        | active_users |
++------------+--------------+ 
+| 2019-07-20 | 2            |
+| 2019-07-21 | 2            |
++------------+--------------+ 
+Explanation: Note that we do not care about days with zero active users.
+
+"""
+
+
+# Solution 
+# Write your MySQL query statement below
+
+'''
+# SOLUTION - 1
+SELECT activity_date AS day, COUNT(distinct user_id) AS active_users 
+FROM Activity 
+WHERE activity_date >= '2019-06-28' AND activity_date <= '2019-07-27'
+GROUP BY activity_date;
+
+
+# SOUTION - 2
+SELECT activity_date day, count(distinct user_id) active_users
+FROM Activity
+WHERE DATEDIFF('2019-07-27', activity_date) BETWEEN 0 AND 29
+GROUP BY activity_date;
+
+'''
+
+# -NOTE- Whatever we use in 'GROUP BY' as we have written in 'SELECT'
